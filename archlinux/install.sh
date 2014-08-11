803c17d405b6987047de015f6b7a015f2a1f985a
jamming <juanjoaguililla@gmail.com>
2014-08-11 02:00:17 +0200

    Supporting wifi and GPT

--- archlinux/install.sh
+++ archlinux/install.sh
@@ -258,9 +258,11 @@ setupInitramdisk () {
 }
 
 setupBootloader () {
-    pacins grub efibootmgr dosfstools dialog iw
-    grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
-    grub-mkconfig -o /boot/grub/grub.cfg
+    #pacins grub efibootmgr dosfstools dialog iw
+    #grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=arch_grub --recheck
+    #grub-mkconfig -o /boot/grub/grub.cfg
+    pacins gummiboot
+    gummiboot install
 }
 
 ##### C O N F I G #####
