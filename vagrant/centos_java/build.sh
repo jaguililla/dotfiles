3cf5936f6888ff44bd319c8cc51f73c1d0a34a82
jamming <juanjoaguililla@gmail.com>
2017-01-19 21:02:04 +0100

    Download VB guest additions in host

--- vagrant/centos_java/build.sh
+++ vagrant/centos_java/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+VBOXVER="5.1.14"                                                                                     
+VBOXDL="http://download.virtualbox.org/virtualbox"
+wget -nc -O vboxga.iso "$VBOXDL/$VBOXVER/VBoxGuestAdditions_$VBOXVER.iso"
+
 vagrant up
 vagrant package
 
