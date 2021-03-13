ee526ae214ca1f7709c14acd045c808ec0a9f60d
jaguililla <juanjoaguililla@gmail.com>
2021-03-13 13:42:32 +0100

    Improve MacOS settings

--- macosx/README.md
+++ macosx/README.md
@@ -1,63 +1,11 @@
 
-Mac OS X install
-----------------
-
+# Mac OS X install
 To install basic packages, run:
 
 To install Docker Toolbox it is necessary to grant permissions in:
 `System Preferences... > Security & Privacy > General` after running the `brew` command.
 
-```bash
-brew unlink docker-compose
-
-brew tap adoptopenjdk/openjdk
-brew tap mongodb/brew
-
-brew install \
-  virtualbox \
-  docker \
-  docker-toolbox \
-  postman \
-  gimp \
-  inkscape \
-  firefox \
-  google-chrome \
-  adoptopenjdk11 \
-  macvim \
-  adoptopenjdk8 \
-  awscli \
-  bash-completion \
-  coreutils \
-  dep \
-  fdupes \
-  git \
-  git-extras \
-  gnu-tar \
-  graphviz \
-  groovy \
-  httpie \
-  jq \
-  kotlin \
-  kubernetes-cli \
-  kubernetes-helm \
-  maven \
-  gradle \
-  mongodb-community \
-  node \
-  ruby \
-  jenv \
-  nnn \
-  taskwarrior-tui \
-  taskwarrior-pomodoro \
-  hammerspoon \
-  neofetch \
-  plantuml \
-  alt-tab \
-  wget
-
-brew link --overwrite docker-compose
-```
-
+## jEnv
 To setup JEnv execute:
 
 ```bash
@@ -72,27 +20,9 @@ jenv enable-plugin export
 The file `ssh_config` can be linked in `$HOME/.ssh/config` to use Keychain to remember passwords.
 I.e.: `ln -s $DOTFILES/macosx/ssh_config $HOME/.ssh/config`
 
-You could add these lines to your `~/.bash_profile` file:
-
-```bash
-source $HOME/Projects/jaguililla/dotfiles/shell/ssh_aliases
-
-source $HOME/Projects/jaguililla/dotfiles/containers/docker_aliases
-source $HOME/Projects/jaguililla/dotfiles/containers/minikube_aliases
-source $HOME/Projects/jaguililla/dotfiles/containers/minishift_aliases
-
-source $HOME/Projects/jaguililla/dotfiles/vagrant/vagrant_aliases
-
-source $HOME/Projects/jaguililla/dotfiles/git/git_aliases
-
-source $HOME/Projects/jaguililla/dotfiles/macosx/bashconfig
-source $HOME/Projects/jaguililla/dotfiles/macosx/aliases
-```
-
 Oh My Zsh: `sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
 
-# Python
-
+## Python
 ```bash
 brew install python@3
 
@@ -100,8 +30,10 @@ brew install python@3
 export PATH=/usr/local/opt/python@3/Frameworks/Python.framework/Versions/Current/bin:$PATH
 ```
 
+## Kernel Parameters
 ```
 sudo sysctl -w kern.maxfiles=24500
 sudo sysctl -w kern.maxfilesperproc=20480
 ```
 
+## Disable Mac
--- macosx/install.sh
+++ macosx/install.sh
@@ -1,29 +1,72 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-export DOTFILES_HOME=${1:-"~/Projects/dotfiles"}
+brew unlink docker-compose
 
-ln -s $DOTFILES_HOME/shell/inputrc ~/.inputrc
-ln -s $DOTFILES_HOME/editors/vim/gvimrc ~/.gvimrc
-ln -s $DOTFILES_HOME/editors/vim/vimrc ~/.vimrc
-ln -s $DOTFILES_HOME/git/gitconfig ~/.gitconfig
+brew tap adoptopenjdk/openjdk
+brew tap mongodb/brew
+
+brew install \
+  virtualbox \
+  docker \
+  docker-toolbox \
+  postman \
+  gimp \
+  inkscape \
+  firefox \
+  google-chrome \
+  adoptopenjdk11 \
+  macvim \
+  adoptopenjdk8 \
+  awscli \
+  bash-completion \
+  coreutils \
+  dep \
+  fdupes \
+  git \
+  git-extras \
+  gnu-tar \
+  graphviz \
+  groovy \
+  httpie \
+  jq \
+  kotlin \
+  kubernetes-cli \
+  kubernetes-helm \
+  maven \
+  gradle \
+  mongodb-community \
+  node \
+  ruby \
+  jenv \
+  nnn \
+  taskwarrior-tui \
+  taskwarrior-pomodoro \
+  hammerspoon \
+  neofetch \
+  plantuml \
+  alt-tab \
+  wget
+
+brew link --overwrite docker-compose
+
+BASEDIR=$(dirname $0)
+BASEPATH=$(readlink -f $BASEDIR 2>/dev/null || greadlink -f $BASEDIR)
+
+export DOTFILES=${1:-"$BASEPATH/.."}
+[[ ! -d $DOTFILES ]] && echo 'DOTFILES must exists' && exit 1
+
+ln -s $DOTFILES/git/gitconfig ~/.gitconfig
 
 echo "alias dircolors='gdircolors'" >>~/.bashrc
-echo "source $DOTFILES_HOME/shell/bashconfig" >>~/.bashrc
-echo "source $DOTFILES_HOME/aws/aws_aliases" >>~/.bashrc
-echo "source $DOTFILES_HOME/shell/aliases" >>~/.bashrc
-echo "source $DOTFILES_HOME/shell/ssh_aliases" >>~/.bashrc
-echo "source $DOTFILES_HOME/containers/docker_aliases" >>~/.bashrc
-echo "source $DOTFILES_HOME/containers/minikube_aliases" >>~/.bashrc
-echo "source $DOTFILES_HOME/git/git_aliases" >>~/.bashrc
-echo "source $DOTFILES_HOME/vagrant/vagrant_aliases" >>~/.bashrc
+echo "source $DOTFILES/aws/aws_aliases" >>~/.bashrc
+echo "source $DOTFILES/containers/docker_aliases" >>~/.bashrc
+echo "source $DOTFILES/containers/minikube_aliases" >>~/.bashrc
+echo "source $DOTFILES/git/git_aliases" >>~/.bashrc
+echo "source $DOTFILES/vagrant/vagrant_aliases" >>~/.bashrc
 
 echo "alias dircolors='gdircolors'" >>~/.zshrc
-echo "source $DOTFILES_HOME/shell/shellconfig" >>~/.zshrc
-echo "source $DOTFILES_HOME/shell/zshconfig" >>~/.zshrc
-echo "source $DOTFILES_HOME/aws/aws_aliases" >>~/.zshrc
-echo "source $DOTFILES_HOME/shell/aliases" >>~/.zshrc
-echo "source $DOTFILES_HOME/shell/ssh_aliases" >>~/.zshrc
-echo "source $DOTFILES_HOME/containers/docker_aliases" >>~/.zshrc
-echo "source $DOTFILES_HOME/containers/minikube_aliases" >>~/.zshrc
-echo "source $DOTFILES_HOME/git/git_aliases" >>~/.zshrc
-echo "source $DOTFILES_HOME/vagrant/vagrant_aliases" >>~/.zshrc
+echo "source $DOTFILES/aws/aws_aliases" >>~/.zshrc
+echo "source $DOTFILES/containers/docker_aliases" >>~/.zshrc
+echo "source $DOTFILES/containers/minikube_aliases" >>~/.zshrc
+echo "source $DOTFILES/git/git_aliases" >>~/.zshrc
+echo "source $DOTFILES/vagrant/vagrant_aliases" >>~/.zshrc
