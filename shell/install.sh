ebd9e8c0903f8758f6307180c51dcb65bb6ed60b
jaguililla <juanjoaguililla@gmail.com>
2022-03-09 18:09:58 +0100

    Update settings

--- shell/install.sh
+++ shell/install.sh
@@ -8,6 +8,7 @@ export DOTFILES=${1:-"$BASEPATH/.."}
 [[ ! -d $DOTFILES/shell ]] && echo 'Shell settings must exists' && exit 2
 
 ln -s $DOTFILES/shell/inputrc ~/.inputrc
+ln -s $DOTFILES/shell/starship.toml ~/.config/starship.toml
 
 curl -s "https://get.sdkman.io" | bash
 
@@ -16,3 +17,8 @@ echo "source $DOTFILES/shell/shellconfig" >>~/.zshrc
 echo "source $DOTFILES/shell/zshconfig" >>~/.zshrc
 echo "source $DOTFILES/shell/aliases" >>~/.zshrc
 echo "source $DOTFILES/shell/ssh_aliases" >>~/.zshrc
+
+echo "export DOTFILES=$DOTFILES" >>~/.bashrc
+echo "source $DOTFILES/shell/shellconfig" >>~/.bashrc
+echo "source $DOTFILES/shell/aliases" >>~/.bashrc
+echo "source $DOTFILES/shell/ssh_aliases" >>~/.bashrc
