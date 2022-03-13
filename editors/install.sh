c9c33c8bce7023de6cdbce85b302a2863a2c942a
jaguililla <juanjoaguililla@gmail.com>
2022-03-13 19:22:48 +0100

    Update Vim install

--- editors/install.sh
+++ editors/install.sh
@@ -8,8 +8,9 @@ export DOTFILES=${1:-"$BASEPATH/.."}
 [[ ! -d $DOTFILES/editors ]] && echo 'Editors settings must exists' && exit 2
 [[ -z $IDEA_HOME ]] && echo 'IntelliJ IDEA home must be defined' && exit 3
 
-ln -s $DOTFILES/editors/vim/gvimrc ~/.gvimrc
-ln -s $DOTFILES/editors/vim/vimrc ~/.vimrc
+mkdir ~/.vim
+ln -s $DOTFILES/editors/vim/gvimrc ~/.vim
+ln -s $DOTFILES/editors/vim/vimrc ~/.vim
 # TODO if Windows, use `~/AppData/Local/nvim`
 ln -s $DOTFILES/editors/vim/init.vim ~/.config/nvim/init.vim
 ln -s $DOTFILES/editors/vim/ginit.vim ~/.config/nvim/ginit.vim
